8dfb9a9c31450ff79e7682236ce5a9d43564e0ab
Ivan Vargas <41218476+IvanVargasG@users.noreply.github.com>
2018-10-21 18:08:45 -0500

    Agregar Index
    Se agrega el index al repositorio

---
+++ index.php
@@ -0,0 +1,54 @@
+		
+		
+		<?php include ('encabezado.php'); ?>
+
+		
+
+
+
+
+		<div id="sfccds4l8nx1afxy324ud6dx43gexlktxmh"></div>
+		<script type="text/javascript" src="https://counter5.wheredoyoucomefrom.ovh/private/counter.js?c=cds4l8nx1afxy324ud6dx43gexlktxmh&down=async" async></script>
+		<noscript><a href="https://www.contadorvisitasgratis.com" title="contadores de paginas web gratuitos"><img src="https://counter5.wheredoyoucomefrom.ovh/private/contadorvisitasgratis.php?c=cds4l8nx1afxy324ud6dx43gexlktxmh" border="0" title="contadores de paginas web gratuitos" alt="contadores de paginas web gratuitos" style="float:right";></a></noscript>
+		
+	
+	
+		<p>
+		<h2>
+			Análisis de Requisitos 
+		</h2>
+			Este OVI consta de 4 secciones la primera denominada <b>"lecturas"</b> esta sección cuenta con 5 lecturas sobre la tematica analisis de requisitos; la segunda sección <b>"multimedia"</b> consta de dos webs con contenido multimedia (videos, audios) hechos por los estudiantes del grupo, para la tercera sección <b>"Actividades"</b>la cual contiene 6 actividades que ayudaran a una mejor retención de la información del OVI y por ultimo la seccion <b>"Autores"</b> donde podra encontrar minibiografias de los estudiantes que realizaron este OVI.<br>
+
+		<h2>
+			Objetivos del OVI
+		</h2>
+
+			<h2>Objetivo general</h2>
+				Conocer la organización y el debido proceso para llevar a cabo un excelente análisis de requisitos conforme al formato de Especificación de Requerimientos de Software (ERS) con el estándar IEEE830
+
+				<h2>Objetivo específico 1 </h2>
+					Reconocer los actores que interactúan en la consolidación de requisitos para un software
+
+					<h2>Objetivo específico 2</h2> 
+						Diferenciar los requerimientos funcionales de los no funcionales además de reconocer como interactúan estos durante el proceso de análisis de requerimientos
+
+						<h2>Objetivo específico 3</h2>
+							Dar a conocer el formato IEEE830 y su correcto diligenciamiento <br>
+
+
+	
+					<a text  class="twitter-timeline" data-lang="es" data-width="500" data-height="500" href="https://twitter.com/UniversidadUNAD?ref_src=twsrc%5Etfw">Tweets by UniversidadUNAD</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>	</p>
+
+				<?php include ('piepag.php');?>
+
+ 			</header>
+    
+
+
+
+
+		</DIV>
+	
+	</body>
+</html>
+
